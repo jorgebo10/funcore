a86209feefec4d0bd4286bb35f8f859414c79981
jorge.bo <jorge.bo@travelx.io>
2023-03-06 17:25:40 -0300

    add serializer for offset dates

---
+++ src/main/kotlin/io/github/jbo/shell/serializer/OffsetDateTimeIso8601Serializer.kt
@@ -0,0 +1,20 @@
+package io.github.jbo.shell.serializer
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.OffsetDateTime
+
+object OffsetDateTimeIso8601Serializer : KSerializer<OffsetDateTime> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("OffsetDateTime", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): OffsetDateTime = OffsetDateTime.parse(decoder.decodeString())
+
+    override fun serialize(encoder: Encoder, value: OffsetDateTime) {
+        encoder.encodeString(value.toString())
+    }
+}
